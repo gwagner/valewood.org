48f02600521c899dabdcfab0530d4c895c68dddf
Geoff Wagner <wageof@gmail.com>
2022-09-05 18:46:30 -0500

    Fix JS

--- webp.mjs
+++ webp.mjs
@@ -21,8 +21,10 @@ jQuery(window).on('load', function() {
                       .getPropertyValue('background-image') !== 'none';
           }).each(function(){
             var bg = jQuery(this).css('background-image');
-            bg = filePath.substring(0,bg.lastIndexOf(".")+1) + "webp";
-            jQuery(this).css("background-image", "url('" + bg + "')");
+            console.log("Before: "+ bg)
+            bg = bg.substring(0,bg.lastIndexOf(".")+1) + "webp\")";
+            console.log("After: "+ bg)
+            jQuery(this).css("background-image", bg);
           });
       }
   };
@@ -30,6 +32,17 @@ jQuery(window).on('load', function() {
 });
 </script>
 `;
+
+jQuery('*').filter(function() {
+  if (this.currentStyle)
+      return this.currentStyle['backgroundImage'] !== 'none';
+  else if (window.getComputedStyle)
+      return document.defaultView.getComputedStyle(this, null)
+          .getPropertyValue('background-image') !== 'none';
+}).each(function(){
+  var bg = jQuery(this).css('background-image');
+  console.log(bg)
+});
 
 // options is optional
 glob("**/*.{jpg,png}", {}, function (er, files) {
