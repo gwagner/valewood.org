3aef550155cf1ac2be5eabd12c2cf03fa9cc8bfe
Geoffrey Wagner <wageof@gmail.com>
2022-09-06 19:53:31 -0500

    finalize build.sh and add header for robots.txt

--- build.sh
+++ build.sh
@@ -8,5 +8,4 @@ npm install --save-dev pages-plugin-asset-negotiation
 
 node webp.mjs
 
-#rm -rf ./node_modules/ webp.mjs package.json package-lock.json docker.sh build.sh update.sh .gitignore
 rm -rf webp.mjs package.json package-lock.json docker.sh build.sh update.sh .gitignore
