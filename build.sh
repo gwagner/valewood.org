39e40c6ecd3dfa53cf66d50a9374f36142d77245
Geoffrey Wagner <wageof@gmail.com>
2022-09-06 19:47:26 -0500

    Test out a middleware?

--- build.sh
+++ build.sh
@@ -4,7 +4,9 @@ echo "Install Node Deps"
 npm install imagemin
 npm install imagemin-webp
 npm install glob
+npm install --save-dev pages-plugin-asset-negotiation
 
 node webp.mjs
 
-rm -rf /work/node_modules/ webp.mjs package.json package-lock.json docker.sh build.sh update.sh .gitignore
+#rm -rf ./node_modules/ webp.mjs package.json package-lock.json docker.sh build.sh update.sh .gitignore
+rm -rf webp.mjs package.json package-lock.json docker.sh build.sh update.sh .gitignore
