6f739a7c062139289ffc074288dc3bc27cf6b73a
Geoff Wagner <wageof@gmail.com>
2022-09-09 12:31:06 -0500

    add search index

--- build.sh
+++ build.sh
@@ -4,8 +4,13 @@ echo "Install Node Deps"
 npm install imagemin
 npm install imagemin-webp
 npm install glob
+npm install lunr
+npm install cheerio
 npm install --save-dev pages-plugin-asset-negotiation
 
 node webp.mjs
 
-rm -rf webp.mjs package.json package-lock.json docker.sh build.sh update.sh .gitignore
+node build_index.mjs
+node create_serp_page.mjs
+
+rm -rf webp.mjs build_index.mjs package.json package-lock.json docker.sh build.sh run.sh update.sh .gitignore
---
+++ functions/index.ts
@@ -0,0 +1,34 @@
+export async function onRequest(context) {
+    // Contents of context object
+    const {
+        request, // same as existing Worker API
+        env, // same as existing Worker API
+        params, // if filename includes [id] or [[path]]
+        waitUntil, // same as ctx.waitUntil in existing Worker API
+        next, // used for middleware or to fetch assets
+        data, // arbitrary space for passing data between middlewares
+    } = context;
+
+    const url = new URL(request.url);
+
+    // bail early if we are not looking at a search
+    if(url.searchParams.get('s') == null)
+        return next();
+
+    const asset = await env.ASSETS.fetch(url.origin + '/search/');
+    if(asset && asset.status === 200){
+        // If we did not get a proper status code back, then bail
+        if([101, 204, 205, 304].includes(asset.status)){
+            return next()
+        }
+
+        console.log(asset)
+
+        // found the asset, so we can serve it
+        return asset;
+    }
+	
+    console.log('fall through')
+	// no acceptable formats found, pass-through for original image
+	return next();
+}
