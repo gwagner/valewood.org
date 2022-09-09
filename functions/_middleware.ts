a7faefce59413e7723c7d89dc8335a4be631bfa6
Geoff Wagner <wageof@gmail.com>
2022-09-09 14:37:44 -0500

    adjustments Add json header and try different method for serving serp pages

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -15,12 +15,12 @@ export async function onRequest(context) {
     if(url.searchParams.get('s') == null)
         return next();
 
-    const asset = await env.ASSETS.fetch(new URL(url.origin + '/search/'));
+    const assetURL = new URL('/search/', request.url).toString();
+    const assetReq = new Request(assetURL, {
+        cf: request.cf
+    });
+    const asset = await env.ASSETS.fetch(assetReq);
     if(asset && asset.status === 200){
-        // If we did not get a proper status code back, then bail
-        if([101, 204, 205, 304].includes(asset.status)){
-            return next()
-        }
 
         // found the asset, so we can serve it
         return asset;
