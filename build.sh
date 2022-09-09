5da095d1d8d779d3d65c96f5b7887c7092232f05
Geoff Wagner <wageof@gmail.com>
2022-09-09 14:53:16 -0500

    update scripts

--- build.sh
+++ build.sh
@@ -8,9 +8,13 @@ npm install lunr
 npm install cheerio
 npm install --save-dev pages-plugin-asset-negotiation
 
+# Generate WebP Images
 node webp.mjs
 
+# Generate search index
 node build_index.mjs
+
+# Generate the SERP page
 node create_serp_page.mjs
 
-rm -rf webp.mjs build_index.mjs package.json package-lock.json docker.sh build.sh run.sh update.sh .gitignore
+rm -rf webp.mjs build_index.mjs package.json package-lock.json build.sh run.sh update.sh .gitignore
