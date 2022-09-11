a45246e1c757c6d71b3f7e24de0ece9c697d9f11
Geoff Wagner <wageof@gmail.com>
2022-09-11 14:20:18 -0500

    Fix atom feeds

--- update.sh
+++ update.sh
@@ -17,8 +17,18 @@ grep -rl www.lab.valewood.org . --exclude='update.sh' | xargs sed -i 's/www.lab.
 echo "Replacing loc>/ with www.valewood.org"
 grep -rl "loc>/" . --exclude='update.sh' | xargs sed -i 's/\(loc>\)\//\1https:\/\/www.valewood.org\//g'
 
+# RSS Atom Feed Fixing
 echo "Replacing link>/ with www.valewood.org"
 grep -rl "link>/" . --exclude='update.sh' | xargs sed -i 's/\(link>\)\//\1https:\/\/www.valewood.org\//g'
+
+echo "Replacing comments>/ with www.valewood.org"
+grep -rl "comments>/" . --exclude='update.sh' | xargs sed -i 's/\(comments>\)\//\1https:\/\/www.valewood.org\//g'
+
+echo "Replacing comments>/ with www.valewood.org"
+grep -rl "commentRss>/" . --exclude='update.sh' | xargs sed -i 's/\(commentRss>\)\//\1https:\/\/www.valewood.org\//g'
+
+echo "Replacing href=\"/ with www.valewood.org"
+grep -rl "href=\"/" . --include="*.xml" | xargs sed -i 's/\(href="\)\//\1https:\/\/www.valewood.org\//g'
 
 echo "Fixing canonical links with https://www.valewood.org"
 grep -rl '<link rel="canonical" href="/' . --exclude='update.sh' | xargs sed -i 's/\(<link rel="canonical" href="\)\//\1https:\/\/www.valewood.org\//g'
