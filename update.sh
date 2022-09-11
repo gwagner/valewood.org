b09b804db411dd6c54e91da869c9a08d536dc77a
Geoff Wagner <wageof@gmail.com>
2022-09-11 14:11:49 -0500

    Fix atom feed links

--- update.sh
+++ update.sh
@@ -14,8 +14,11 @@ rm -rf ./new-site/
 echo "Replacing www.lab.valewood.org with www.valewood.org"
 grep -rl www.lab.valewood.org . --exclude='update.sh' | xargs sed -i 's/www.lab.valewood.org/www.valewood.org/g'
 
-echo "Replacing <loc>/ with www.valewood.org"
+echo "Replacing loc>/ with www.valewood.org"
 grep -rl "loc>/" . --exclude='update.sh' | xargs sed -i 's/\(loc>\)\//\1https:\/\/www.valewood.org\//g'
+
+echo "Replacing link>/ with www.valewood.org"
+grep -rl "link>/" . --exclude='update.sh' | xargs sed -i 's/\(link>\)\//\1https:\/\/www.valewood.org\//g'
 
 echo "Fixing canonical links with https://www.valewood.org"
 grep -rl '<link rel="canonical" href="/' . --exclude='update.sh' | xargs sed -i 's/\(<link rel="canonical" href="\)\//\1https:\/\/www.valewood.org\//g'
